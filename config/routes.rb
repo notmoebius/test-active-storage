291b271810774cd67a88711c6eb8bb3137dfcb03
notmoebius <notmoebius@gmail.com>
2019-05-10 11:45:40 +0200

    init commit for production

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get 'users/show'
+  resources :users, only: [:show] do
+    resources :avatars, only: [:create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
